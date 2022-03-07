b2edd82afe6c40a76dfb2e0cd54d4d8f5864403f
umar100 <aumar1665@gmail.com>
2022-03-07 13:47:03 +0100

    re

---
+++ src/innerclasses/Button.java
@@ -0,0 +1,22 @@
+package innerclasses;
+
+public class Button {
+    private String title;
+    private OnClickListener onClickLister;
+
+    public Button(String title) {
+        this.title = title;
+    }
+
+    public void setOnClickListener(OnClickListener onClickListener) {
+        this.onClickLister = onClickListener;
+    }
+
+    public void onClick() {
+        this.onClickLister.onClick(this.title);
+    }
+
+    public interface OnClickListener {
+        public void onClick(String title);
+    }
+}
